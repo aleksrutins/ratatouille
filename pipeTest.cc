cb56449c44197c5a1b422008549c8ccdd516cc83
munchkinhalfling <munchkin@rutins.com>
2019-08-21 12:10:32 -0400

    Why did it not work?

--- pipeTest.cc
+++ pipeTest.cc
@@ -9,6 +9,7 @@ class PipeableClass {
         val = "Hi";
     }
     void addWorld() {
+        cout << "adding world";
         val += " world";
     }
     string get() {
@@ -26,9 +27,11 @@ int main() {
     })->ret();
     Ratatouille::Pipe::Piper<PipeableClass> *otherPiper = new Ratatouille::Pipe::Piper<PipeableClass>(PipeableClass());
     otherPiper->pipe([](PipeableClass p) {
+        cout << "first";
         cout << p.get();
         return p;
     })->callMemberFn(&PipeableClass::addWorld)->pipe([](PipeableClass p) {
+        cout << "second";
         cout << p.get();
         return p;
     })->ret();
